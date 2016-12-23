97d918f9cf75ccff986460858cbb0c9570965606
Michal Klos <michal.m.klos@gmail.com>
2016-12-23 05:20:26 +0100

    cleanup

--- Source/ShaderPluginDemo/ShaderPluginDemoCharacter.cpp
+++ Source/ShaderPluginDemo/ShaderPluginDemoCharacter.cpp
@@ -11,7 +11,8 @@ DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
 // AShaderPluginDemoCharacter
 
 AShaderPluginDemoCharacter::AShaderPluginDemoCharacter() {
-    // Set size for collision capsule
+    
+	// Set size for collision capsule
     GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
 
     // set our turn rates for input
@@ -19,16 +20,13 @@ AShaderPluginDemoCharacter::AShaderPluginDemoCharacter() {
     BaseLookUpRate = 45.f;
 
     // Create a CameraComponent
-    FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>
-                                 (TEXT("FirstPersonCamera"));
+    FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
     FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
-    FirstPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f,
-            64.f); // Position the camera
+    FirstPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); // Position the camera
     FirstPersonCameraComponent->bUsePawnControlRotation = true;
 
     // Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
-    Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>
-             (TEXT("CharacterMesh1P"));
+    Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
     Mesh1P->SetOnlyOwnerSee(true);
     Mesh1P->SetupAttachment(FirstPersonCameraComponent);
     Mesh1P->bCastDynamicShadow = false;
@@ -38,14 +36,12 @@ AShaderPluginDemoCharacter::AShaderPluginDemoCharacter() {
 
     // Create a gun mesh component
     FP_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
-    FP_Gun->SetOnlyOwnerSee(
-        true);          // only the owning player will see this mesh
+    FP_Gun->SetOnlyOwnerSee(true);          // only the owning player will see this mesh
     FP_Gun->bCastDynamicShadow = false;
     FP_Gun->CastShadow = false;
     //FP_Gun->AttachTo(Mesh1P, TEXT("GripPoint"), EAttachLocation::SnapToTargetIncludingScale, true);
 
-    FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>
-                        (TEXT("MuzzleLocation"));
+    FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
     FP_MuzzleLocation->SetupAttachment(FP_Gun);
     FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
 
@@ -54,14 +50,6 @@ AShaderPluginDemoCharacter::AShaderPluginDemoCharacter() {
 
     // Note: The ProjectileClass and the skeletal mesh/anim blueprints for Mesh1P are set in the
     // derived blueprint asset named MyCharacter (to avoid direct content references in C++)
-
-
-
-
-
-
-
-
 
 
 
@@ -80,14 +68,10 @@ AShaderPluginDemoCharacter::AShaderPluginDemoCharacter() {
 void AShaderPluginDemoCharacter::BeginPlay() {
     // Call the base class
     Super::BeginPlay();
-    FP_Gun->AttachToComponent(Mesh1P,
-                              FAttachmentTransformRules::SnapToTargetIncludingScale,
-                              TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
+    FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules::SnapToTargetIncludingScale, TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
 
-    PixelShading = new FPixelShaderUsageExample(PixelShaderTopLeftColor,
-            GetWorld()->Scene->GetFeatureLevel());
-    ComputeShading = new FComputeShaderUsageExample(ComputeShaderSimulationSpeed,
-            1024, 1024, GetWorld()->Scene->GetFeatureLevel());
+    PixelShading = new FPixelShaderUsageExample(PixelShaderTopLeftColor, GetWorld()->Scene->GetFeatureLevel());
+    ComputeShading = new FComputeShaderUsageExample(ComputeShaderSimulationSpeed,1024, 1024, GetWorld()->Scene->GetFeatureLevel());
 }
 
 //Do not forget cleanup :)
@@ -121,8 +105,7 @@ void AShaderPluginDemoCharacter::Tick(float DeltaSeconds) {
     TotalElapsedTime += DeltaSeconds;
 
     if (PixelShading) {
-        EndColorBuildup = FMath::Clamp(EndColorBuildup + DeltaSeconds *
-                                       EndColorBuildupDirection, 0.0f, 1.0f);
+        EndColorBuildup = FMath::Clamp(EndColorBuildup + DeltaSeconds * EndColorBuildupDirection, 0.0f, 1.0f);
 
         if (EndColorBuildup >= 1.0 || EndColorBuildup <= 0) {
             EndColorBuildupDirection *= -1;
@@ -133,19 +116,17 @@ void AShaderPluginDemoCharacter::Tick(float DeltaSeconds) {
 
         if (ComputeShading) {
             ComputeShading->ExecuteComputeShader(TotalElapsedTime);
-            InputTexture =
-                ComputeShading->GetTexture(); //This is the output texture from the compute shader that we will pass to the pixel shader.
+            InputTexture = ComputeShading->GetTexture(); //This is the output texture from the compute shader that we will pass to the pixel shader.
         }
 
-        ComputeShaderBlend = FMath::Clamp(ComputeShaderBlend +
-                                          ComputeShaderBlendScalar * DeltaSeconds, 0.0f, 1.0f);
-        PixelShading->ExecutePixelShader(RenderTarget, InputTexture,
-                                         FColor(EndColorBuildup * 255, 0, 0, 255), ComputeShaderBlend);
+        ComputeShaderBlend = FMath::Clamp(ComputeShaderBlend + ComputeShaderBlendScalar * DeltaSeconds, 0.0f, 1.0f);
+        PixelShading->ExecutePixelShader(RenderTarget, InputTexture, FColor(EndColorBuildup * 255, 0, 0, 255), ComputeShaderBlend);
     }
 }
 
 void AShaderPluginDemoCharacter::OnFire() {
-    //Try to set a texture to the object we hit!
+
+	//Try to set a texture to the object we hit!
     FHitResult HitResult;
     FVector StartLocation = FirstPersonCameraComponent->GetComponentLocation();
     FRotator Direction = FirstPersonCameraComponent->GetComponentRotation();
@@ -153,10 +134,9 @@ void AShaderPluginDemoCharacter::OnFire() {
     FCollisionQueryParams QueryParams;
     QueryParams.AddIgnoredActor(this);
 
-    if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation,
-            ECC_Visibility, QueryParams)) {
-        TArray<UStaticMeshComponent*> StaticMeshComponents =
-            TArray<UStaticMeshComponent*>();
+    if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams)) {
+
+        TArray<UStaticMeshComponent*> StaticMeshComponents = TArray<UStaticMeshComponent*>();
         AActor* HitActor = HitResult.GetActor();
 
         if (NULL != HitActor) {
@@ -165,8 +145,7 @@ void AShaderPluginDemoCharacter::OnFire() {
             for (int32 i = 0; i < StaticMeshComponents.Num(); i++) {
                 UStaticMeshComponent* CurrentStaticMeshPtr = StaticMeshComponents[i];
                 CurrentStaticMeshPtr->SetMaterial(0, MaterialToApplyToClickedObject);
-                UMaterialInstanceDynamic* MID =
-                    CurrentStaticMeshPtr->CreateAndSetMaterialInstanceDynamic(0);
+                UMaterialInstanceDynamic* MID = CurrentStaticMeshPtr->CreateAndSetMaterialInstanceDynamic(0);
                 UTexture* CastedRenderTarget = Cast<UTexture>(RenderTarget);
                 MID->SetTextureParameterValue("InputTexture", CastedRenderTarget);
             }
@@ -195,34 +174,10 @@ void AShaderPluginDemoCharacter::SetupPlayerInputComponent(
     check(InputComponent);
 
     //ShaderPluginDemo Specific input mappings
-    InputComponent->BindAction("SavePixelShaderOutput", IE_Pressed, this,
-                               &AShaderPluginDemoCharacter::SavePixelShaderOutput);
-    InputComponent->BindAction("SaveComputeShaderOutput", IE_Pressed, this,
-                               &AShaderPluginDemoCharacter::SaveComputeShaderOutput);
-    InputComponent->BindAxis("ComputeShaderBlend", this,
-                             &AShaderPluginDemoCharacter::ModifyComputeShaderBlend);
+    InputComponent->BindAction("SavePixelShaderOutput", IE_Pressed, this, &AShaderPluginDemoCharacter::SavePixelShaderOutput);
+    InputComponent->BindAction("SaveComputeShaderOutput", IE_Pressed, this, &AShaderPluginDemoCharacter::SaveComputeShaderOutput);
+    InputComponent->BindAxis("ComputeShaderBlend", this, &AShaderPluginDemoCharacter::ModifyComputeShaderBlend);
     //ShaderPluginDemo Specific input mappings
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -233,30 +188,24 @@ void AShaderPluginDemoCharacter::SetupPlayerInputComponent(
     InputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
     //InputComponent->BindTouch(EInputEvent::IE_Pressed, this, &AShaderPluginDemoCharacter::TouchStarted);
-    if (EnableTouchscreenMovement(InputComponent) == false) {
-        InputComponent->BindAction("Fire", IE_Pressed, this,
-                                   &AShaderPluginDemoCharacter::OnFire);
+    if (!EnableTouchscreenMovement(InputComponent)) {
+        InputComponent->BindAction("Fire", IE_Pressed, this, &AShaderPluginDemoCharacter::OnFire);
     }
 
-    InputComponent->BindAxis("MoveForward", this,
-                             &AShaderPluginDemoCharacter::MoveForward);
-    InputComponent->BindAxis("MoveRight", this,
-                             &AShaderPluginDemoCharacter::MoveRight);
+    InputComponent->BindAxis("MoveForward", this, &AShaderPluginDemoCharacter::MoveForward);
+    InputComponent->BindAxis("MoveRight", this, &AShaderPluginDemoCharacter::MoveRight);
 
     // We have 2 versions of the rotation bindings to handle different kinds of devices differently
     // "turn" handles devices that provide an absolute delta, such as a mouse.
     // "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
     InputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-    InputComponent->BindAxis("TurnRate", this,
-                             &AShaderPluginDemoCharacter::TurnAtRate);
+    InputComponent->BindAxis("TurnRate", this,  &AShaderPluginDemoCharacter::TurnAtRate);
     InputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-    InputComponent->BindAxis("LookUpRate", this,
-                             &AShaderPluginDemoCharacter::LookUpAtRate);
+    InputComponent->BindAxis("LookUpRate", this, &AShaderPluginDemoCharacter::LookUpAtRate);
 }
 
-void AShaderPluginDemoCharacter::BeginTouch(const ETouchIndex::Type FingerIndex,
-        const FVector Location) {
-    if (TouchItem.bIsPressed == true) {
+void AShaderPluginDemoCharacter::BeginTouch(const ETouchIndex::Type FingerIndex, const FVector Location) {
+    if (TouchItem.bIsPressed) {
         return;
     }
 
@@ -266,22 +215,21 @@ void AShaderPluginDemoCharacter::BeginTouch(const ETouchIndex::Type FingerIndex,
     TouchItem.bMoved = false;
 }
 
-void AShaderPluginDemoCharacter::EndTouch(const ETouchIndex::Type FingerIndex,
-        const FVector Location) {
-    if (TouchItem.bIsPressed == false) {
+void AShaderPluginDemoCharacter::EndTouch(const ETouchIndex::Type FingerIndex,  const FVector Location) {
+    if (!TouchItem.bIsPressed) {
         return;
     }
 
-    if ((FingerIndex == TouchItem.FingerIndex) && (TouchItem.bMoved == false)) {
+    if ((FingerIndex == TouchItem.FingerIndex) && (!TouchItem.bMoved)) {
         OnFire();
     }
 
     TouchItem.bIsPressed = false;
 }
 
-void AShaderPluginDemoCharacter::TouchUpdate(const ETouchIndex::Type
-        FingerIndex, const FVector Location) {
-    if ((TouchItem.bIsPressed == true) && (TouchItem.FingerIndex == FingerIndex)) {
+void AShaderPluginDemoCharacter::TouchUpdate(const ETouchIndex::Type FingerIndex, const FVector Location) {
+
+    if (TouchItem.bIsPressed && (TouchItem.FingerIndex == FingerIndex)) {
         if (TouchItem.bIsPressed) {
             if (GetWorld() != nullptr) {
                 UGameViewportClient* ViewportClient = GetWorld()->GetGameViewport();
@@ -337,19 +285,17 @@ void AShaderPluginDemoCharacter::LookUpAtRate(float Rate) {
     AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
-bool AShaderPluginDemoCharacter::EnableTouchscreenMovement(
-    class UInputComponent* InputComponent) {
+bool AShaderPluginDemoCharacter::EnableTouchscreenMovement( class UInputComponent* InputComponent) {
+
     bool bResult = false;
 
     if (FPlatformMisc::GetUseVirtualJoysticks() ||
-            GetDefault<UInputSettings>()->bUseMouseForTouch) {
+        GetDefault<UInputSettings>()->bUseMouseForTouch) {
+
         bResult = true;
-        InputComponent->BindTouch(EInputEvent::IE_Pressed, this,
-                                  &AShaderPluginDemoCharacter::BeginTouch);
-        InputComponent->BindTouch(EInputEvent::IE_Released, this,
-                                  &AShaderPluginDemoCharacter::EndTouch);
-        InputComponent->BindTouch(EInputEvent::IE_Repeat, this,
-                                  &AShaderPluginDemoCharacter::TouchUpdate);
+        InputComponent->BindTouch(EInputEvent::IE_Pressed, this, &AShaderPluginDemoCharacter::BeginTouch);
+        InputComponent->BindTouch(EInputEvent::IE_Released, this, &AShaderPluginDemoCharacter::EndTouch);
+        InputComponent->BindTouch(EInputEvent::IE_Repeat, this, &AShaderPluginDemoCharacter::TouchUpdate);
     }
 
     return bResult;
